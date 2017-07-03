cb1ff0c11959d8bdcf9600dbec7af0d324f61a2d
wk <you@example.com>
2017-07-03 16:39:15 +0800

    123

---
+++ MyLibs/MyLibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLibs
+//
+//  Created by WangKun on 2017/7/3.
+//  Copyright © 2017年 WangKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLibs/MyLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLibs
+//
+//  Created by WangKun on 2017/7/3.
+//  Copyright © 2017年 WangKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
