3fee462cad1a2be01dbcb11bf52e287fe43d49be
wk <you@example.com>
2017-07-30 20:06:08 +0800

    111

--- MyLibs/MyLibs/AppDelegate.m
+++ MyLibs/MyLibs/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     //1233
+    //4444
     // Override point for customization after application launch.
     return YES;
 }
